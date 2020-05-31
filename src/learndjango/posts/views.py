03a3729b5d6790dffb62dd82e7baac4cb4cbdb24
Md Asif Iqbal <maxasif@gmail.com>
2020-05-31 19:09:05 +0530

    Create API

---
+++ src/learndjango/posts/views.py
@@ -0,0 +1,44 @@
+from .models import Post
+from .serializers import PostSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format)
+    })
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
